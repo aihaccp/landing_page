4d26a529997d5b687f7f8db4ab9a91208f49ca82
davidlgabriel <david.g.leal00@gmail.com>
2023-08-12 04:19:57 +0100

    vai

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 $host = $_SERVER["HTTP_HOST"];
 header("Location: https://{$host}/index.html");
+
+if (strpos($_SERVER['PHP_SELF'], "/index.html") != false) {
+    die("Acesso negado");
+  }
 exit;
